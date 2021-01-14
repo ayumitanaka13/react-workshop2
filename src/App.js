d01c5d13b217cbe638551e5e4c98547eb9bbfb6d
ayumitanaka <tanakayumi13@gmail.com>
2021-01-13 16:01:33 -0800

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
+
+//import axios from 'axios';
 
 
 function App() {
 
   /* #1 - practice useState */
   
-  // Normal varialble
+  // Normal varialble (javascript / not rendering)
   let normalVariable = 0;
-  const setNormalVariable= () => {
+  const setNormalVariable = () => {
     normalVariable = normalVariable + 1;
     console.log(normalVariable)
   }
 
-  // State of React
+  // State of React (react / rendering(change html))
+  // const [initialValue(変更したい値), setValue] = [0, f]
   const [count, setCount] = useState(0);
   const setStateVariable = () => {
     setCount(count + 1);
@@ -25,10 +28,41 @@ function App() {
     setInputName(event.target.value)
   }
 
+  /* #2 - practice useEffect */
+
+  // useEffect works after redering
+  // useEffect (arrow function, [empty array])
+
+  useEffect(() => {
+    // alert("stop");
+    // document.getElementById("card").style.background = "bisque";
+    // document.getElementById("text").style.color = "orange";
+
+    // axios.get("https://jsonplaceholder.typicode.com/posts")
+    //   .then(res => console.log(res))
+    //   .catch(err => console.log("customer message " + err))
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => console.log("customer message " + err))
+  }, [])
+
+
+
+  // useState with data
+  const [data, setData] = useState([])
+
+
+
   return (
     <div className="container">
-      <div className="card">
-        <div>
+
+      {/* old card */}
+
+      {/*
+      <div className="card" id="card">
+        <div id="text">
           <p>hello world</p>
           <h1>Title</h1>
           <p>content</p>
@@ -37,12 +71,23 @@ function App() {
           <button onClick={setStateVariable}>StateCount</button>
           <div>{`NormalVariable: ${normalVariable}`}</div>
           <div>{`StateVariable: ${count}`}</div>
-          {/* Get input value */}
           <br />
           <input type="text" onChange={onChangeHandler} placeholder="Get value test"/>
           <div>{inputName}</div>
         </div>
       </div>
+      */}
+
+      {/* new card */}
+      {data.map(el => (
+        <div className="card" id="card">
+        <div id="text">
+          <p>{el.id}</p>
+          <h2>{el.title}</h2>
+          <p>{el.body}</p>
+        </div>
+      </div>
+      ))}
     </div>
   );
 }
